a324485c8a9c69fc8996afe2e481187c950d774e
saikatCV <saikat030703@gmail.com>
2024-03-19 08:34:26 +0530

    UC2 : added the functionality to add the new contact details

---
+++ AddEditDeleteContact.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddresBook
+{
+    public class AddEditDeleteContact
+    {
+        public void AddContactDetails()
+        {
+            // writing the logic to add contact in address
+            Console.WriteLine("enter the firstname");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("enter the lastname");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("enter the email");
+            string email = Console.ReadLine();
+            Console.WriteLine("enter the phone");
+            string phone = Console.ReadLine();
+            Console.WriteLine("enter the address");
+            string address = Console.ReadLine();
+            Console.WriteLine("enter the city ");
+            string city = Console.ReadLine();
+            Console.WriteLine("enter the state");
+            string state = Console.ReadLine();
+            Console.WriteLine("enter the zip");
+            string zip = Console.ReadLine();
+            ContactList.Add(new Contactdetails(firstname, lastname, email, phone, address, city, state, zip));
+
+
+
+        }
+
+        public void EditContactDetails()
+
+        { // logic for the editing the user data 
+
+           
+            
+
+        }
+
+        public void DeleteContactDetails()
+        {
+            // logic for deleting the a contact detail of user 
+
+           
+
+                }
+
+        static ArrayList ContactList = new ArrayList();
+        static ArrayList ContactListRemove = new ArrayList();
+
+    }
+
+}
+
+
+
+
